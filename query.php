5d15eb23da4b2362e1121cf34c595abed4bbb669
ellenmschmidt <ellenmschmidt@gmail.com>
2012-03-12 23:06:04 -0300

    Update query.php

--- query.php
+++ query.php
@@ -65,7 +65,7 @@ callAHAH('content.php?content= '+tab, 'content', 'getting content for tab '+tab+
 <input type="text" name="pos_hg18"
 	value= "<?php echo htmlentities($_POST['pos_hg18']);?>" > </p>
 <p><input type="submit" value="Submit">
-<a href="index.html">Cancel</a></p>
+<a href="homepage.php">Cancel</a></p>
 </form>
 
 </div >
