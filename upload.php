323ca36b82cfe3c1235115c65a91b5bdabdfd5d9
kjgrossmann <kjgrossmann@gmail.com>
2012-03-27 14:42:05 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,6 +2,24 @@
 require_once "db.php";
 session_start();
 
+if (  isset($_POST['PMID']) && isset($_POST['First_author'])) {
+	echo 'something';
+	$PMID = mysql_real_escape_string($_POST['PMID']);
+	$First_author = mysql_real_escape_string($_POST['First_author']);
+	$journal = mysql_real_escape_string($_POST['journal']);
+	$pub_year = mysql_real_escape_string($_POST['pub_year']);
+	$title = mysql_real_escape_string($_POST['title']);
+	$trait = mysql_real_escape_string($_POST['trait']);
+	$sql = "INSERT INTO Publications (PMID, First_author, journal, pub_year, title, trait) VALUES ('$PMID', '$First_author', '$journal', '$pub_year', '$title', '$trait')";
+	mysql_query($sql);
+	echo 'blah';
+	$_SESSION['success'] = 'Publication Added';
+
+	return;
+	}
+
+
+
 /*Still to add:
 -ability to strip away first line if it lists tab headings
 -reject a file if it's not properly formatted
@@ -51,6 +69,15 @@ callAHAH('content.php?content= '+tab, 'content', 'getting content for tab '+tab+
 	<div class="page">
 <!-- BEGIN PAGE HEADER AND NAVIGATION -->
 	<div class="g918">	
+<div class="secondaryNav">
+	<?php if ($_SESSION['username'])
+	{
+	echo "<p> Logged in as: ";
+	echo(htmlentities($_SESSION['username']));
+	echo "&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"logout.php\">Logout</a></p>";
+	}
+	?>
+	</div>
 	<div class="top">
 	<div id ="name"><a href="index.php"><span>Home</span></a></div>
 	<h1> CardioGeniDB</h1>	
@@ -58,13 +85,10 @@ callAHAH('content.php?content= '+tab, 'content', 'getting content for tab '+tab+
 	<ul id="tabmenu">
 	<li id ="tab1" > <a href="#" class="active">Upload</a></li>
 	<li id ="tab2"> <a href="query.php" >Query </a></li>
-	<?php if ($_SESSION['username'])
-	{
-	echo "<p> <a href=\"logout.php\">Logout</a></p>";
-	}
-	?>
+	
 	</ul>
 	</div>
+	
 	</div>
 	</div>
 <!-- END PAGE HEADER AND NAVIGATION -->
@@ -76,13 +100,17 @@ callAHAH('content.php?content= '+tab, 'content', 'getting content for tab '+tab+
 	<div class = "g306">
 	<p>
 		<span>Upload your GWAS Results </span>  
-		Registered users may upload association study results files with a 
+		</br>You must be logged in to contribute. If you are not yet logged in, click <a href="login.php"> here</a> to log in. 
+		If you are not yet a member, register <a href="register.php">here</a>. 
+	</p>
+	<p>
+		<br/>Add to the database. Submit publication information and upload association study results files with a 
 		maximum size limit of 20Mb. This allows for a gzipped text table including key columns 
 		(marker name, p-value, standard error, effect size, and sample size) 
-		for up to ~3 million SNPs.<br> <br>
+		for up to ~3 million SNPs.</p><p>
 		
 		In addition to uploading your results file, please fill in the provided 
-		metadata fields with the accompanying publication information.<br><br>
+		metadata fields with the accompanying publication information.</p><p>
 		
 		Your uploaded results will be publicly accessible to everyone.
 	</p>
@@ -96,6 +124,18 @@ callAHAH('content.php?content= '+tab, 'content', 'getting content for tab '+tab+
 <!-- BEGIN UPLOAD STUFF -->
 
 <div class="upload">
+
+
+
+
+
+<form enctype='multipart/form-data' action='upload.php' method='post'>
+	<div class="fieldSet">
+
+<fieldset>
+<legend>Upload Data File</legend>
+<p><label for "username">Please paste properly-formatted .csv file:
+</br>
 <?php
 /*Necessary to ensure proper upload into the database of variously formatted csv
 files -pasj*/
@@ -107,7 +147,7 @@ if (isset($_POST['submit'])) {
 		/*The below displays the contents of the file, probably should not
 		be kept, especially if the files are huge -pasj*/
 		echo "<h2>Displaying contents:</h2>";
-		readfile($_FILES['filename']['tmp_name']);
+		/*readfile($_FILES['filename']['tmp_name']);*/
 	}
  
 	//Import uploaded file to Database
@@ -124,21 +164,66 @@ if (isset($_POST['submit'])) {
 	print "<p>Import done</p>";
  
 	//view upload form
-}else {
- 
-	print "<p>Upload your data.  <big>DO IT.</big></p>";
- 
-	print "<form enctype='multipart/form-data' action='upload.php' method='post'>";
- 
-	print "<p>File name to import:</p>";
- 
-	print "<input size='50' type='file' name='filename'><br />\n";
- 
-	print "<input type='submit' name='submit' value='Upload'></form>";
- 
 }
  
 ?>
+
+
+
+
+<input size='50' type='file' name='filename'><br />
+
+</br>
+</label></p>
+<!--<input type="submit" name="username" <?php 
+	echo 'value="' .htmlentities($_POST['username']) .'"';
+	?>></p>-->
+
+</fieldset>
+		<fieldset>
+		<legend>Publication Information</legend>
+<table class="form_table">
+<tr><td>
+		<label for "PMID">Pubmed ID:
+</td><td>
+		<input type="text" name="PMID" <?php 
+	echo 'value="' .htmlentities($_POST['PMID']) .'"';
+	?>></label>
+</td><tr><td>
+		<label for "First_author">First Author's Last Name, First Initials:</br>(Example: Speliotes, EK for EK Speliotes)  
+</td><td>
+<input type="text" name="First_author" <?php 
+	echo 'value="' .htmlentities($_POST['First_author']) .'"';
+	?>>
+		</label>
+</td></tr><tr><td>
+		<label for "journal">Journal:
+</td><td>
+		<input type="text" name="journal" <?php 
+	echo 'value="' .htmlentities($_POST['journal']) .'"';
+	?>></label>
+</td></tr><tr><td>
+		<label for "pub_year">Year of publication: 
+</td><td>
+		<input type="text" name="pub_year" <?php 
+	echo 'value="' .htmlentities($_POST['pub_year']) .'"';
+	?>></label>
+</td></tr><tr><td>
+		<label for "trait">Trait: 
+</td><td>
+		<input type="text" name="trait" <?php 
+	echo 'value="' .htmlentities($_POST['trait']) .'"';
+	?>> </label>
+</td>
+</tr>
+</table>
+
+</fieldset>
+
+</div>
+<input type="submit" value="Upload" />
+<input type="button" name="Cancel" value="Cancel" onclick="window.location = 'upload.php' " /> 
+</form>
 </div>
 
 <!-- END UPLOAD STUFF -->
