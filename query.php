8953a3e65df9da00e6c5bade4f01d3f6e4447661
ellenmschmidt <ellenmschmidt@gmail.com>
2012-04-03 14:46:21 -0300

    Update query.php

--- query.php
+++ query.php
@@ -52,7 +52,8 @@ callAHAH('content.php?content= '+tab, 'content', 'getting content for tab '+tab+
 	<div class = "g306">
 	<p>
 		<span>Query GWAS data </span>  
-		</br>Use the provided fields to query the existing database for your variant of interest. 
+		</br>Query for your variant of interest from the current database using the following fields.
+		First select a trait, and then fill in one or more of the following fields to narrow your search to a single genetic variant.
 	</p>
 	
 	<div class="clear">&nbsp;</div>
@@ -64,44 +65,53 @@ callAHAH('content.php?content= '+tab, 'content', 'getting content for tab '+tab+
 	<div class ="g612">
 <fieldset>
 <legend>Query</legend>
-<p>Query data from the current database using one or more of the following fields:</p>
-
-<form method="post">
+<br></br>
+<form method="POST">
+<p>Trait:
+	<select name="trait">
+	<option value="BMI">BMI</option>
+	<option value="BP">BP</option>
+	<option value="Fasting Glucose">Fasting Glucose</option>
+	<option value="Fasting Proinsulin">Fasting Proinsulin</option>
+	</select> <br> *Note: Trait is a required field</p>
+	<!--<input type="text" name="trait"
+	value= "<?php echo htmlentities($_POST['trait']);?>" > </p>-->
 <p>MarkerName (rsID):
 <input type="text" 
        name="MarkerName" 
        value="<?php echo htmlentities($_POST['MarkerName']);?>" > </p>
-<p>Chromosome:
-<select name="chr">
-	<option value="<?php echo htmlentities($_POST['chr']);?>">1</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">2</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">3</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">4</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">5</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">6</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">7</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">8</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">9</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">10</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">11</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">12</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">13</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">14</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">15</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">16</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">17</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">18</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">19</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">20</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">21</option>
-	<option value="<?php echo htmlentities($_POST['chr']);?>">22</option>
-</select> </p>
 <p>Position (hg18):
 <input type="text" name="pos_hg18"
 	value= "<?php echo htmlentities($_POST['pos_hg18']);?>" > </p>
 <p>Position (hg19):
 	<input type="text" name="pos_hg19"
 	value= "<?php echo htmlentities($_POST['pos_hg19']);?>" > </p>
+	<p>Chromosome:
+<select name="chr">
+	<option value="chr">chr</option>
+	<option value="1">1</option>
+	<option value="2">2</option>
+	<option value="3">3</option>
+	<option value="4">4</option>
+	<option value="5">5</option>
+	<option value="6">6</option>
+	<option value="7">7</option>
+	<option value="8">8</option>
+	<option value="9">9</option>
+	<option value="10">10</option>
+	<option value="11">11</option>
+	<option value="12">12</option>
+	<option value="13">13</option>
+	<option value="14">14</option>
+	<option value="15">15</option>
+	<option value="16">16</option>
+	<option value="17">17</option>
+	<option value="18">18</option>
+	<option value="19">19</option>
+	<option value="20">20</option>
+	<option value="21">21</option>
+	<option value="22">22</option>
+</select><br> *Note: Selecting chromosome will output all SNPs on that chromosome. This query may take several minutes.</p>
 <p><input type="submit" value="Submit">
 <input type="button" name="Cancel" value="Cancel" onclick="window.location = 'query.php' " /> 
 
@@ -113,18 +123,19 @@ callAHAH('content.php?content= '+tab, 'content', 'getting content for tab '+tab+
 	<div class="clear">&nbsp;</div>
 	
 <?php
-if (  isset($_POST['MarkerName']) && isset($_POST['pos_hg18']) && isset($_POST['pos_hg19']) && isset($_POST['chr'])) {
+if (  isset($_POST['MarkerName']) && isset($_POST['pos_hg18']) && isset($_POST['pos_hg19']) && isset($_POST['chr']) && isset($_POST['trait'])) {
 	$markername = mysql_real_escape_string($_POST['MarkerName']);
 	$pos_hg18 = mysql_real_escape_string($_POST['pos_hg18']);
 	$pos_hg19 = mysql_real_escape_string($_POST['pos_hg19']);
 	$chr = mysql_real_escape_string($_POST['chr']);
+	$trait = mysql_real_escape_string($_POST['trait']);
 	$sql = 
-	"SELECT chr, INFO.pos_hg18, INFO.pos_hg19, INFO.MarkerName,
-		BMI.p,trait,First_author,Journal,pub_year,title,Publications.PMID
-		FROM INFO, BMI, Publications
-		WHERE BMI.PMID = Publications.PMID AND
-		BMI.MarkerName = INFO.MarkerName AND
-		(INFO.chr= '$chr' OR INFO.pos_hg18 = '$pos_hg18' OR INFO.pos_hg19 = '$pos_hg19' OR INFO.MarkerName = '$markername')";
+	"SELECT INFO.chr, INFO.pos_hg18, INFO.pos_hg19, INFO.MarkerName,
+		results.p,trait,First_author,Journal,pub_year,title,Publications.PMID
+		FROM INFO, results, Publications
+		WHERE results.PMID = Publications.PMID AND
+		results.MarkerName = INFO.MarkerName AND trait='$trait' AND
+		(chr='$chr' OR INFO.pos_hg18 = '$pos_hg18' OR INFO.pos_hg19 = '$pos_hg19' OR INFO.MarkerName = '$markername')";
 	echo '<table  border="1"> <br> 
 <tr>
 	<th>Chr</th>
