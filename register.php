42ba121726c7b8564aa926898f7882511c1ffbc0
kjgrossmann <kjgrossmann@gmail.com>
2012-04-05 16:15:37 -0300

    Added javascript for form validation.

--- register.php
+++ register.php
@@ -2,10 +2,11 @@
 require_once "db.php";
 session_start();
 
-if (  isset($_POST['username']) && isset($_POST['password'])) {
+if (  isset($_POST['username']) && isset($_POST['password']) && isset($_POST['username'])) {
 	$username = mysql_real_escape_string($_POST['username']);
 	$password = mysql_real_escape_string($_POST['password']);
-	$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+	$email = mysql_real_escape_string($_POST['email']);
+	$sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
 	mysql_query($sql);
 	$_SESSION['success'] = 'User Added';
 	header( 'Location: upload.php' ) ;
@@ -69,22 +70,68 @@ callAHAH('content.php?content= '+tab, 'content', 'getting content for tab '+tab+
 <!-- ADD USER STUFF ON RIGHT-->
 	<div class ="g612">
 
+<script type="text/javascript">
+<!-- JAVASCRIPT FOR FORM VALIDATION -->
+function Validate()
+{
+  var IsValid = true;
+
+  document.getElementById("UserNameERR").innerHTML = "";
+  document.getElementById("PasswordERR").innerHTML = "";
+  document.getElementById("EmailERR").innerHTML = "";
+
+  // Check for a name
+  if (document.getElementById("username").value == "") {
+    document.getElementById("UserNameERR").innerHTML = "Missing name";
+    IsValid = false;
+  }
+  // Check for an address
+  if (document.getElementById("password").value == "") {
+    document.getElementById("PasswordERR").innerHTML = "Missing password";
+    IsValid = false;
+  }	
+  // Check for a valid email address
+  var ValidEmail = false;
+  for (i=0; i<document.getElementById("email").value.length; i++) {
+    if (document.getElementById("email").value.charAt(i) == "@") {
+      ValidEmail = true;
+      break;
+    }
+  }
+  if (ValidEmail == false) {		
+    document.getElementById("EmailERR").innerHTML = "Incorrect email format";
+    IsValid = false;
+  }	
+
+  return IsValid;
+
+}
+
+</script>
 
 <fieldset>
 <legend>Add a new user account:</legend>
 
-<form method="post">
+<form name="MyForm" method="post" onsubmit="return Validate()">
 <p>Username:
-<input type="text" name="username" <?php 
+<input type="text" name="username" id="username" <?php 
 	echo 'value="' .htmlentities($_POST['username']) .'"';
-	?>></p>
+	?>><span id="UserNameERR" style="color:red"></span></p>
 <p>Password:
-<input type="password" name="password"<?php 
+<input type="password" name="password" id="password"<?php 
 	echo 'value="' .htmlentities($_POST['password']) .'"';
-	?>></p>
-<p><input type="submit" value="Submit"/>
-<input type="button" name="Cancel" value="Cancel" onclick="window.location = 'upload.php' " /> 
+	?>><span id="PasswordERR" style="color:red"></p>
+<p>Email: 
+<input type="text" name="email" id="email"<?php 
+	echo 'value="' .htmlentities($_POST['email']) .'"';
+	?>><span id="EmailERR" style="color:red"></span></p>
+	
+	<p><input type="submit" value="Submit"/>
+<input type="reset" name="Cancel" value="Cancel" onclick="window.location = 'upload.php' " /> 
+
+<!--for later: <span id="EmailERR" style="color:red"></span> -->
 </form>
+</fieldset>
 
 </div >
 	<div class="clear">&nbsp;</div>
